25c7c373c24e7730e2de0f67714e8f83f3dd8c52
will.Washington <william.washington@live.com>
2019-05-14 23:10:49 -0400

    :this is working well :it does everything I want it to except delete the user directory from the users folder on the root of C :i get an access denied error when trying
    :i bet SYSTEM can do it - need to test in the field

--- ProfClean/Program.cs
+++ ProfClean/Program.cs
@@ -3,12 +3,11 @@ using System.Runtime.InteropServices;
 using System.Security.Principal;
 using System.Management;
 using System.Management.Automation;
-using System.Windows;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading;
 using System.DirectoryServices;
 using Microsoft.Win32;
+using System.IO;
+using System.DirectoryServices.ActiveDirectory;
 
 namespace ProfClean
 {
@@ -28,92 +27,74 @@ namespace ProfClean
     {
         static void Main(string[] args)
         {
-            string regPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
-            Console.WriteLine(Registry.GetValue(regPath, "", ""));
-
-            foreach (string name in GetLocalUserNames())
-            {
-                DirectoryEntry localDirectory = new DirectoryEntry("WinNT://" + Environment.MachineName.ToString());
-                DirectoryEntries users = localDirectory.Children;
-                DirectoryEntry user = users.Find($"{name}");
-                users.Remove(user);
-                //working - need to remove dirs and maybe regkeys - hopefully reg keys are gone
-                Console.WriteLine(name);
-            }
-
-            foreach (string sid in GetLocalUserSids())
-            {
-                
-            }
-
-
+            CleanUpAccounts();
+            Console.WriteLine("test read");
             Console.Read();
-        }
-
-       internal static void Error_DataAdded(object sender, DataAddedEventArgs e)
-        {           
-            Console.WriteLine("An error was written to the Error stream! " + e.ToString());
-        }
+        }    
 
         //build list of users on the computer
-        private static List<string> GetLocalUserNames()
+        private static void CleanUpAccounts()
         {
-            List<string> usernameList = new List<string>();
-            List<string> sidList = new List<string>();
-            SelectQuery query = new SelectQuery("Win32_UserProfile");
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
-            foreach (ManagementObject sid in searcher.Get())
-            {
-                Console.WriteLine(sid);
-                //string user = new SecurityIdentifier(sid["SID"].ToString()).Translate(typeof(NTAccount)).ToString();
-                //int pos = user.LastIndexOf("\\") + 1;
-                //string username = user.Substring(pos, user.Length - pos);                
-                //if (username != "NEO" && username != "NETWORK SERVICE" && username != "LOCAL SERVICE" && username != "SYSTEM")
-                //    //replace with ed_admin
-                //{
-                //    Console.WriteLine(username);
-                //    NTAccount f = new NTAccount(username);
-                //    SecurityIdentifier s = (SecurityIdentifier)f.Translate(typeof(SecurityIdentifier));
-                //    String sidString = s.ToString();
-                //    sidList.Add(sidString);
-                //    usernameList.Add(username);
-                //}                
-            }
-            return usernameList;
-        }
-
-        //the error I was getting was caused because this application deleted the user from the machine but did not scrub the registry
-        //this syntax:
-        ////string user = new SecurityIdentifier(sid["SID"].ToString()).Translate(typeof(NTAccount)).ToString();
-        //queries the registry to find users, so it could see the sid but not the user on the machine
-        //this is fixed now
-
-        //foreach user, need to delete registry key by sid and then delete user folder by username
-
-        
-
-        private static List<string> GetLocalUserSids()
-        {
-            
-            List<string> sidList = new List<string>();
             SelectQuery query = new SelectQuery("Win32_UserProfile");
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
             foreach (ManagementObject sid in searcher.Get())
             {
                 string user = new SecurityIdentifier(sid["SID"].ToString()).Translate(typeof(NTAccount)).ToString();
                 int pos = user.LastIndexOf("\\") + 1;
-                string username = user.Substring(pos, user.Length - pos);
+                string username = user.Substring(pos, user.Length - pos);                
                 if (username != "NEO" && username != "NETWORK SERVICE" && username != "LOCAL SERVICE" && username != "SYSTEM")
                 //replace with ed_admin
                 {
-                    Console.WriteLine(username);
                     NTAccount f = new NTAccount(username);
                     SecurityIdentifier s = (SecurityIdentifier)f.Translate(typeof(SecurityIdentifier));
                     String sidString = s.ToString();
-                    sidList.Add(sidString);                    
+                    RemoveUserRegistryEntry(sidString);
+                    RemoveLocalUser(username);                    
+                }
+            }            
+        }
+
+        private static void RemoveUserRegistryEntry(string sidString)
+        {
+            string keyName = $"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\ProfileList";
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(keyName, true))
+            {
+                Console.WriteLine("deleted registry key " + sidString);
+                Console.WriteLine("test1");
+                if (key != null)
+                {
+                    key.DeleteSubKey(sidString);
                 }
             }
-            return sidList;
+        }
+
+        private static void RemoveLocalUser(string username)
+        {
+            string dir = $"C:\\Users\\{username}";
+            DirectoryEntry localDirectory = new DirectoryEntry("WinNT://" + Environment.MachineName.ToString());
+            DirectoryEntries users = localDirectory.Children;
+            DirectoryEntry user = users.Find($"{username}");
+            users.Remove(user); //remove the user account from the pc
+
+            //if (Directory.Exists(dir))
+            //{
+            //    Console.WriteLine("Delete dir " + username);
+            //    ClearAttributes(dir);
+            //    Directory.Delete(dir, true);
+            //}
+            //this is not working because of read-only but I bet system when ran as KACE will do it - need to test at work
+            //will evaluate tomorrow
+        }
+
+        private static void ClearAttributes(string currentDir)
+        {
+            var di = new DirectoryInfo(currentDir);        
+            di.Attributes &= ~FileAttributes.ReadOnly;          
+        }
+
+        private static void Error_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            Console.WriteLine("An error was written to the Error stream! " + e.ToString());
         }
     }
 
